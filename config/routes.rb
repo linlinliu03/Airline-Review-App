685cd134e3d726c65830efd6d55d8d862ee4f8b5
linlinliu <linlinliu03@gmail.com>
2020-06-08 10:14:04 -0700

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#root'
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end 
+
+  get '*path', to: 'pages#index', via: :all
 end
