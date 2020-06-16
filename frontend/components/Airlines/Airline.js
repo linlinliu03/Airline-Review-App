1e4408a684bad071fe1e2acb4408bffd2e9fc956
linlinliu <linlinliu03@gmail.com>
2020-06-16 11:51:00 -0700

    Add airline list item component

---
+++ frontend/components/Airlines/Airline.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Airlines from "./Airlines";
+
+
+const Airline = (props) => {
+   return(
+       <div className="card">
+          <div className="airline-logo">
+              <img src={props.attributes.image_url} alt={props.attributes.name} />
+          </div>
+          <div className="airline-name">{props.attributes.name}</div>
+          <div className="airline-score">{props.attributes.avg_score}</div>
+          <div className="airline-link">
+               <a href={`/airlines/${props.attributes.slug}`}>View Airline</a>
+          </div>
+       </div>
+   )
+}
+
+export default Airline;
