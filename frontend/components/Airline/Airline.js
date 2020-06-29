6a3d7b422dfe36b26d9f4dbbfeb08b4bab4a2761
linlinliu <linlinliu03@gmail.com>
2020-06-29 11:32:11 -0700

    Format Airline component

--- frontend/components/Airline/Airline.js
+++ frontend/components/Airline/Airline.js
@@ -15,7 +15,15 @@ const Airline = (props) => {
     }, [])
 
     return (
-        <div>This is our Airline show component!</div>
+        <div className="wrapper">
+            <div className="column">
+                <div className="header"></div>
+                <div className="reviews"></div>
+            </div>
+            <div className="column">
+                <div className="review-form"></div>
+            </div>
+        </div>
     )
 }
 
