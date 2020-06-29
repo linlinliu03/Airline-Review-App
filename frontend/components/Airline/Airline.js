92960308e5a53bf3918cc9211051a86927871f1d
linlinliu <linlinliu03@gmail.com>
2020-06-29 11:28:58 -0700

    Fetch data for individula airline

--- frontend/components/Airline/Airline.js
+++ frontend/components/Airline/Airline.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-const Airline = () => {
+const Airline = (props) => {
+    const [airline, setAirline] = useState({});
+    const [reviews, setReviews] = useState({});
+
+    useEffect(() => {
+       const slug = props.match.params.slug;
+       const url = `/api/v1/airlines/${slug}`;
+
+       axios.get(url)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <div>This is our Airline show component!</div>
     )
