b4cf10e3571d58f90aad7fdd89927c33226fca48
linlinliu <linlinliu03@gmail.com>
2020-07-01 19:27:19 -0700

    Add style to Header component

--- frontend/components/Airline/Header.js
+++ frontend/components/Airline/Header.js
@@ -1,17 +1,42 @@
 import React from "react";
+import style from "styled-components";
+
+const Wrapper = style.div`
+        padding: 50px 100px 50px 0;
+        font-size: 30px;
+
+        img{
+            height: 60px;
+            width: 60px;
+            border-radius: 100%;
+            border: 1px solid rgba(0,0,0, 0.1);
+            margin-bottom: -8px;
+        }
+`
+
+const TotalReviews = style.div`
+        font-size: 18px;
+        padding: 10px 0;
+`
+
+const TotalOutOf = style.div`
+        font-size: 18px;
+        font-weight: bold;
+        padding: 10px 0;
+`
 
 const Header = (props) => {
-    const {avege_score, name, image_url} = props.attributes;
+    const {avg_score, name, image_url} = props.attributes;
     const total = props.reviews.length;
     return(
-        <div className="wrapper">
-            <h1><img src={image_url}/>{name}</h1>
+        <Wrapper>
+            <h1><img src={image_url}/> {name}</h1>
             <div>
-                <div className="totalReviews">{total} User Reviews</div>
+                <TotalReviews>{total} User Reviews</TotalReviews>
                 <div className="starRating"></div>
-                <div className="totalOutOf"> {avege_score} out of 5</div>
+                <TotalOutOf> {total} out of 5</TotalOutOf>
             </div>
-        </div>
+        </Wrapper>
     )
 }
 
