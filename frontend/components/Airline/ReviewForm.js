c2cf99e6d0c1c7eb9e54f7b8a966e4778ea5b179
linlinliu <linlinliu03@gmail.com>
2020-07-02 22:54:01 -0700

    Add elements on ReviewForm

--- frontend/components/Airline/ReviewForm.js
+++ frontend/components/Airline/ReviewForm.js
@@ -2,7 +2,24 @@ import React from "react";
 
 const ReviewForm = (props) => {
     return (
-        <div></div>
+        <div className="Wrapper">
+           <form>
+               <div>Have an experience with [Airlin Name]? Share your review!</div>
+               <div className="field">
+                  <input type="text" name="title" placeholder="Review Title"/>
+               </div>
+                <div className="field">
+                    <input type="text" name="description" placeholder="Review Description" />
+                </div>
+                <div className="field">
+                    <div className="rating-container">
+                         <div className="rating-title-text">Rate This Airline</div>
+                         [Star Rating Goes Here]
+                    </div>
+                </div>
+               <button type="submit">Submit Your Review</button>
+           </form>
+        </div>
     )
 }
 
